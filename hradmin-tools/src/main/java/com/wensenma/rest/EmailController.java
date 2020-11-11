dbe34a745e4fb013e724e2b106f2aec5c718cce6
WensenMa-Git <wensen_ma@hotmail.com>
2020-11-12 01:16:23 +0800

    I just evaluated random code in my console

---
+++ hradmin-tools/src/main/java/com/wensenma/rest/EmailController.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright 2019-2020 Wensen Ma
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.wensenma.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import com.wensenma.annotation.Log;
+import com.wensenma.domain.vo.EmailVo;
+import com.wensenma.domain.EmailConfig;
+import com.wensenma.service.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 发送邮件
+ * @author Wensen Ma
+ * @date 2018/09/28 6:55:53
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/email")
+@Api(tags = "工具：邮件管理")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @GetMapping
+    public ResponseEntity<Object> queryConfig(){
+        return new ResponseEntity<>(emailService.find(),HttpStatus.OK);
+    }
+
+    @Log("配置邮件")
+    @PutMapping
+    @ApiOperation("配置邮件")
+    public ResponseEntity<Object> updateConfig(@Validated @RequestBody EmailConfig emailConfig) throws Exception {
+        emailService.config(emailConfig,emailService.find());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Log("发送邮件")
+    @PostMapping
+    @ApiOperation("发送邮件")
+    public ResponseEntity<Object> sendEmail(@Validated @RequestBody EmailVo emailVo){
+        emailService.send(emailVo,emailService.find());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
